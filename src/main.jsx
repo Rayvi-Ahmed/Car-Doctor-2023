ed77d168c7194a26b7ac7b8614fcd945d9e98c0d
Misbah Uddin <rayvi73@gmail.com>
2023-05-12 21:12:47 +0600

    auth context use

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
+import { AuthContext } from './AuthProvider/AuthProvider';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='mx-auto max-w-7xl '>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <AuthContext>
+        <RouterProvider router={router} />
+      </AuthContext>
     </React.StrictMode>,
   </div>
 )
